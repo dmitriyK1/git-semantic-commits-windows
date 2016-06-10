dac19b9708a1b9a965ea67a04ab63d75b6f50481
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2016-06-10 17:14:48 +0300

    style: format git-semantic output

--- git-extensions/git-semantic.cmd
+++ git-extensions/git-semantic.cmd
@@ -1,24 +1,24 @@
 @echo:
 
-@echo (type)(scope): (subject)
+@echo    type (scope): subject
 
 @echo:
 
-@echo git feat     : new feature
-@echo git docs     : changes to documentation
-@echo git chore    : updating grunt tasks etc; no production code change
-@echo git fix      : bug fix
-@echo git refactor : refactoring production code
-@echo git style    : formatting, missing semi colons, etc; no code change
-@echo git test     : adding missing tests, refactoring tests; no production code change
-@echo git localize : localization
+@echo    git feat     : new feature
+@echo    git docs     : changes to documentation
+@echo    git chore    : updating grunt tasks etc; no production code change
+@echo    git fix      : bug fix
+@echo    git refactor : refactoring production code
+@echo    git style    : formatting, missing semi colons, etc; no code change
+@echo    git test     : adding missing tests, refactoring tests; no production code change
+@echo    git localize : localization
 
 @echo:
 
-@echo Example (scope) values:
-@echo init
-@echo runner
-@echo watcher
-@echo config
-@echo web-server
-@echo proxy
+@echo    Example (scope) values:
+@echo        init
+@echo        runner
+@echo        watcher
+@echo        config
+@echo        web-server
+@echo        proxy
